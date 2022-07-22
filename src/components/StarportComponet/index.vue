2ca558844d12f6d83affd6fa3ea627f514c8ac7e
zhuhaobam <bamboo123love@163.com>
2022-07-22 09:44:11 +0800

    fix: 更新

--- src/components/StarportComponet/index.vue
+++ src/components/StarportComponet/index.vue
@@ -31,15 +31,11 @@ const props = withDefaults(defineProps<Props>(), {
   index: 0
 })
 const { index } = toRefs(props)
-const src = ref('')
 const images: any[] = [
   'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1113%2F0F420110430%2F200F4110430-6-1200.jpg&amp;refer=http%3A%2F%2Fimg.jj20.com&amp;app=2002&amp;size=f9999,10000&amp;q=a80&amp;n=0&amp;g=0n&amp;fmt=auto?sec=1658570176&amp;t=b0baf8fc004c0ba24a0d2fac73afcb5d',
   'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1113%2F0F420110430%2F200F4110430-6-1200.jpg&amp;refer=http%3A%2F%2Fimg.jj20.com&amp;app=2002&amp;size=f9999,10000&amp;q=a80&amp;n=0&amp;g=0n&amp;fmt=auto?sec=1658570176&amp;t=b0baf8fc004c0ba24a0d2fac73afcb5d'
 ]
-watch(index, (newValue, oldValue) => {
-  src.value = images[newValue]
-})
-
+const src = ref(images[index.value])
 const counter = ref(Math.round(Math.random() * 100))
 </script>
 <style lang="scss" scoped></style>
